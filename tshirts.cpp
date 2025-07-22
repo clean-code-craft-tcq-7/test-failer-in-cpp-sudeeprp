cb3e4d6a74c981cdc10f2764afaff72ccbbf3f82
Sudeep Prasad <sudeep.rp@gmail.com>
2025-07-22 12:19:44 +0530

    strengthen tshirts test

--- tshirts.cpp
+++ tshirts.cpp
@@ -9,13 +9,14 @@ char size(int cms) {
         sizeName = 'M';
     } else if(cms > 42) {
         sizeName = 'L';
-    }
-    return sizeName;
+    } // PROBLEM: No else here
+    return sizeName; // PROBLEM: null character for size 38
 }
 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
     assert(size(37) == 'S');
+    assert(size(38) == 'S');
     assert(size(40) == 'M');
     assert(size(43) == 'L');
     std::cout << "All is well (maybe!)\n";
