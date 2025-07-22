be53deeac064dc9d6f226d946a728e730ac25f0b
Sudeep <sudeep.rp@gmail.com>
2025-07-22 12:51:34 +0530

    stong asserts for misaligned

--- misaligned.cpp
+++ misaligned.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <assert.h>
 
+int pair_number(int i, int j) {
+    return i * 5 + j;
+}
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            std::cout << i * 5 + j << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
+            std::cout << pair_number(i, j) << " | " << majorColor[i] << " | " << minorColor[i] << "\n";
         }
     }
     return i * j;
@@ -17,5 +21,8 @@ void testPrintColorMap() {
     std::cout << "\nPrint color map test\n"; 
     int result = printColorMap();
     assert(result == 25);
+    assert(pair_number(0, 0) == 1);
+    assert(pair_number(0, 1) == 2);
+    assert(pair_number(4, 4) == 25);
     std::cout << "All is well (maybe!)\n";
 }
